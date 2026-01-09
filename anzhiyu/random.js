8ecc04b396c748ad721a77bccadb2f237c416bfb
lumu999 <18837225887.@163.com>
2026-01-09 11:07:37 +0800

    Site updated: 2026-01-09 11:07:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/3357.html","posts/2700.html","posts/3290.html","posts/df2a.html","posts/b26f.html","posts/337a.html","posts/ab5b.html","posts/c5be.html","posts/fb4c.html","posts/94a6.html"];function toRandomPost(){
+var posts=["posts/b26f.html","posts/3357.html","posts/2700.html","posts/3290.html","posts/df2a.html","posts/337a.html","posts/c5be.html","posts/ab5b.html","posts/fb4c.html","posts/94a6.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
